50e6c0a5062bea522fddbb9b24e8f79d279b7d26
Abdykali Azat <azat.abdykalli@gmail.com>
2024-11-13 00:25:39 +0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://tulparcargo-site.kz:3001',
+   
+    // nameCargo: 'TURKESTAN KARGO'
+    nameCargo: 'Tulpar Cargo'
+  };
+  
+  
+export default config;
